9a178c23e28858b043e569b46df65ae4769b738b
Russell Briggs <russ@paraflyer.net>
2019-09-25 11:26:19 +1200

    Added directory walk and deletion of .pyc files

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This tool is designed to help migrate custom modules from OpenERP (primarily v7)
 to Odoo 12+. It may be useful for conversions between other versions too -
 contributions welcome!
 
+# Usage
+
+We recommend first running python's `2to3` command line utility first, then:
+
+```
+oerp2odoo <module_path>
+```
+
 # License
 
 MIT
--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
+import os
+from os import path
 
-def oerp2odoo(path):
-    print(path)
+
+def oerp2odoo(mod_path):
+    print(f"Converting module in {mod_path} ...")
+
+    for dir_name, subdir_list, file_list in os.walk(mod_path):
+        print(f"Directory: {dir_name}")
+
+        for file_name in file_list:
+            file_ext = path.splitext(file_name)[1]
+
+            if file_ext.lower() == ".pyc":
+                print(f" - Deleting: {file_name}")
+                os.remove(path.join(dir_name, file_name))
+            else:
+                print(f" - Unchanged: {file_name}")
